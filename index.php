abd9dee5de2daf7210fbcf396eea5fc23202d620
kelegorm <a.perez@ctrlb.es>
2021-05-28 10:24:10 +0200

    Empezando el index

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+
+    <!-- Compiled and minified JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    <title>Control Horario CtrlB</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo">CtrlB Control Horario</a>
+        </div>
+    </nav>
+    <h1>Control Horario CtrlB</h1>
+    <div class="autosignarea">
+        <p>¿Has trabajado hoy en tu horario normal?</p>
+        <button class="autosignbtn">si</button>
+    </div>
+    <div class="customsign">
+        <p>Selecciona el Horario</p>
+        <form method="POST" action="">
+        
+        </form>
+    </div>
+</body>
+</html>
