3141228dae499cb52ea244802adba9b349822e6a
kelegorm <a.perez@ctrlb.es>
2021-05-30 10:41:26 +0200

    Modificacion indx

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php
+
+//horario verano/Invierno
+if (5 < date("n") and date("n") < 10) {
+    $hoursStr = 'startWork1=0800&endWork1=1500&startWork2=0000&endWork2=0000';
+}else{
+    $hoursStr = 'startWork1=0800&endWork1=1400&startWork2=1500&endWork2=1900';
+}
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -13,15 +23,27 @@
 </head>
 <body>
     <nav>
-        <div class="nav-wrapper">
+        <div class="nav-wrapper black">
             <a href="#" class="brand-logo">CtrlB Control Horario</a>
         </div>
     </nav>
-    <h1>Control Horario CtrlB</h1>
-    <div class="autosignarea">
-        <p>¿Has trabajado hoy en tu horario normal?</p>
-        <button class="autosignbtn">si</button>
+
+
+    <div class="row">
+    <div class="col s12 m6">
+      <div class="card blue-grey darken-1">
+        <div class="card-content white-text">
+          <span class="card-title">Firma Rápida</span>
+          <p>Si has hecho tu horario normal, por favor confirma aqui. Juana te lo agradecerá.</p>
+        </div>
+        <div class="card-action">
+          <a href="/control_horario/add.php?worker=<?=$_GET['worker']?>&<?=$hoursStr?>">Confirmar</a>
+        </div>
+      </div>
     </div>
+  </div>
+
+
     <div class="customsign">
         <p>Selecciona el Horario</p>
         <form method="POST" action="">
